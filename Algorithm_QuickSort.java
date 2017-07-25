27db55a1310ba3429be859e90f628ff3eb62ea93
Peng Li <pengli@email.com>
2017-07-24 21:52:06 -0700

    Add algorithm quicksort

---
+++ Algorithm_QuickSort.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Algorithm_QuickSort {
+    public static void quicksort(int[] arr) {
+        quicksort(arr, 0, arr.length - 1);
+    }
+    public static void quicksort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quicksort(arr, low, pivot - 1);
+            quicksort(arr, pivot + 1, high);
+        }
+    }
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                swap(arr, ++i, j);
+            }
+        }
+        swap(arr, ++i, high);
+        return i;
+    }
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void main(String[] args) {
+        int[] arr = {9,8,7,6,5,4,3,2,1};
+        quicksort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
